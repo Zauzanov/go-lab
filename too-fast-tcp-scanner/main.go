54924b50adcf4eac8a26597661151fc2ea8591cd
Zauzanov <zauzanov@gmail.com>
2026-02-10 03:59:39 +0300

    Add comments

--- too-fast-tcp-scanner/main.go
+++ too-fast-tcp-scanner/main.go
@@ -9,7 +9,7 @@ func main() {
 	for i := 1; i <= 1024; i++ {
 		go func(j int) {
 			address := fmt.Sprintf("scanme.nmap.org:%d", j)
-			conn, err := net.Dial("tcp", address)
+			conn, err := net.Dial("tcp", address) // We wrap the Dial(network, address string) call in a goroutine.
 			if err != nil {
 				return
 			}
