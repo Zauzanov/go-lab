87ba297cfffb3c20781f3fbb271d8f7da65720cf
Zauzanov <zauzanov@gmail.com>
2026-02-11 02:29:13 +0300

    Add comments

--- too-fast-tcp-scanner/main.go
+++ too-fast-tcp-scanner/main.go
@@ -1,5 +1,7 @@
 package main
 
+// This version adds concurremcy (goroutines).
+
 import (
 	"fmt"
 	"net"
@@ -7,14 +9,15 @@ import (
 
 func main() {
 	for i := 1; i <= 1024; i++ {
-		go func(j int) {
+		go func(j int) { // Add a new goroutine: a concurrent execution thread. It takes a port number.
+			// It runs this function concurrently for each i.
 			address := fmt.Sprintf("scanme.nmap.org:%d", j)
-			conn, err := net.Dial("tcp", address) // We wrap the Dial(network, address string) call in a goroutine.
-			if err != nil {
+			conn, err := net.Dial("tcp", address) // We wrap the Dial(network, address string) call in a goroutine, so many dials happen at once.
+			if err != nil {                       // If dialing failed, the goroutine exits immediately via return.
 				return
 			}
 			conn.Close()
-			fmt.Printf("%d open\n", j)
-		}(i)
+			fmt.Printf("%d open\n", j) // Uses j (the goroutine’s copy), not i.
+		}(i) // Immediately calls the anonymous function. It passes i as the argument, so inside the goroutine: j gets the value of i at that moment. So each goroutine gets its own port number snapshot.
 	}
 }
