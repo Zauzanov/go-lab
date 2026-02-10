f5dd8a046aa270622d200e4fab8775d6b90fa24f
Zauzanov <zauzanov@gmail.com>
2026-02-11 02:54:37 +0300

    Add comments

--- waitgroup-2fast-tcp-scanner/main.go
+++ waitgroup-2fast-tcp-scanner/main.go
@@ -9,26 +9,35 @@ package main
 // and blocking the main program until all workers report they are finished.
 // It ensures the program doesn't exit before all tasks are complete
 
+// Without goroutines: check port 1, wait, check port 2, wait… (slow)
+// With goroutines: kick off checks for many ports at once (fast). It doesn't block, it runs in the background while we continue.
+
 import (
 	"fmt"
 	"net"
-	"sync"
+	"sync" // synchronization primitives (WaitGroup)
 )
 
 func main() {
-	var wg sync.WaitGroup
+	var wg sync.WaitGroup // Declares a variable named wg, where type is sync.WaitGroup.
 	for i := 1; i <= 1024; i++ {
-		wg.Add(1)
+		wg.Add(1) // Increments the WaitGroup’s internal counter by 1.
 		go func(j int) {
-			defer wg.Done()
+			defer wg.Done() // defer schedules a call to run when the surrounding function returns.
+			// wg.Done() subtracts 1 from the WaitGroup counter.
+			// No matter how this goroutine exits(ok, error...), it marks it finished.
+			// This is important because we do return on error later.
 			address := fmt.Sprintf("scanme.nmap.org:%d", j)
 			conn, err := net.Dial("tcp", address)
 			if err != nil {
-				return
+				return // Because we used defer wg.Done(), the WaitGroup counter still gets decremented even on this early return.
 			}
 			conn.Close()
 			fmt.Printf("%d open\n", j)
 		}(i)
 	}
-	wg.Wait()
+	wg.Wait() // Don’t exit until every goroutine has called Done().
+	// Blocks main() here until the WaitGroup counter returns to 0.
+	// main waits for them all to finish via wg.Wait().
+	// If main exits, goroutines die. That’s why we used a WaitGroup (wg.Wait()), to keep the program alive until all scans finish.
 }
