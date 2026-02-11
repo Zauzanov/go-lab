3139c71783b2746dc3627260414158b37aa8aacd
Zauzanov <zauzanov@gmail.com>
2026-02-11 03:27:08 +0300

    Add comments

--- tcp-sync-scanner/README.md
+++ tcp-sync-scanner/README.md
@@ -1,1 +1,3 @@
 Port scanning using a worker pool is an effective way to manage concurrent network requests without exhausting system resources. The pattern utilizes goroutines and channels to distribute scanning tasks among a fixed number of workers. 
+
+`main.go` is a worker pool pattern usinga buffered channel + a Waitgroup. 
--- tcp-sync-scanner/main.go
+++ tcp-sync-scanner/main.go
@@ -5,10 +5,12 @@ import (
 	"sync"
 )
 
-func worker(ports chan int, wg *sync.WaitGroup) {
-	for p := range ports {
-		fmt.Println(p)
-		wg.Done()
+func worker(ports chan int, wg *sync.WaitGroup) { // a bidirectional channel(can receive and send; inside worker only receives) that carries int values (port numbers).
+	// Plus a pointer to a WaitGroup — all workers share and update the same WaitGroup.
+	for p := range ports { // Reads from the channel in a loop. range ports keeps receiving values until the channel is closed and drained. Each received value is assigned to p.
+		// It keeps working as long as there are port numbers coming in.
+		fmt.Println(p) // Prints the port number p.
+		wg.Done()      // Decrements the WaitGroup counter by 1, signaling that it finished processing one job.
 	}
 }
 
