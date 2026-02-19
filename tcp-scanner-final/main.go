74649fcd0573f0bf985f15e0312ae99b58cde2d9
Zauzanov <zauzanov@gmail.com>
2026-02-19 08:03:42 +0300

    Add main function

--- tcp-scanner-final/main.go
+++ tcp-scanner-final/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"sort"
 )
 
 func worker(ports, results chan int) {
@@ -25,5 +26,25 @@ func main() {
 
 	for i := 0; i < cap(ports); i++ {
 		go worker(ports, results)
+	}
+
+	go func() {
+		for i := 1; i <= 1024; i++ {
+			ports <- i
+		}
+	}()
+
+	for i := 0; i < 1024; i++ {
+		port := <-results
+		if port != 0 {
+			openports = append(openports, port)
+		}
+	}
+
+	close(ports)
+	close(results)
+	sort.Ints(openports)
+	for _, port := range openports {
+		fmt.Printf("%d open\n", port)
 	}
 }
