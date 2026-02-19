0330619328af537606d48755958220de7dbc557e
Zauzanov <zauzanov@gmail.com>
2026-02-19 07:53:33 +0300

    Add worker function

--- tcp-scanner-final/main.go
+++ tcp-scanner-final/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func worker(ports, results chan int) {
+	for p := range ports {
+		address := fmt.Sprintf("scanme.nmap.org:%d", p)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			results <- 0
+			continue
+		}
+		conn.Close()
+		results <- p
+	}
+}
